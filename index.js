9401cb88823dfabab45e5daf93cf45f9c621174d
Bestun Gardi <bestun95@yahoo.com>
2018-08-31 08:53:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file
 function findMatching(arrayOfDrivers, anyString) {
   arrayOfDrivers.filter((el)=> {
-    let x = {}
-    if(el.toLowerCase()== anyString.toLowerCase())
-    {}
-}}
+
+return    el.toLowerCase()== anyString.toLowerCase()})
+}
