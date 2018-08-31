b5011938b1200738aaa524234a463ac303d5e037
Bestun Gardi <bestun95@yahoo.com>
2018-08-31 08:55:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file
 function findMatching(arrayOfDrivers, anyString) {
-  arrayOfDrivers.filter((el)=> {return    el.toLowerCase() === anyString.toLowerCase()
+  return arrayOfDrivers.filter((el)=> {return    el.toLowerCase() === anyString.toLowerCase()
 })
 }
+
