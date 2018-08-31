62ed7a5f1a6de44747266a87f7ec7a5c53e14570
Bestun Gardi <bestun95@yahoo.com>
2018-08-31 08:48:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
-function findMatching(arrayOfDrivers) {
-  filter
+function findMatching(arrayOfDrivers, anyString) {
+  arrayOfDrivers.filter((el)=> el.toLowercase()== anyString.toLowercase())
 }
